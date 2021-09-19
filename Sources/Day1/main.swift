f1464d270a003ff64559f777ea6f19c7a08304f5
Igor Francesco <igorfrancesco2@gmail.com>
2021-09-19 10:18:34 -0300

    ok

---
+++ Sources/Day1/main.swift
@@ -0,0 +1,19 @@
+/*
+if CommandLine.arguments.count != 2 {
+    print("Usage: hello NAME")
+} else {
+    let name = CommandLine.arguments[1]
+    sayHello(name: name)
+}
+*/
+
+
+var str = "Hello, man!";
+str = """
+Goodbye
+Olá Mundo
+""";
+print(str);
+
+
+
